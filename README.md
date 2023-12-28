b3d1b279d2f548ecf37efc64fc889426d107c60d
Michal Jensko <michaljensko@gmail.com>
2023-12-28 18:53:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# QSARDB_ds_compiler
+Tool for creating ready-to-go QSAR datasets in XML format 
---
+++ main.py
@@ -0,0 +1,14 @@
+
+def main():
+    """
+    Main function of the whole tool overseeing process of DS compilling from .txt files contating values of descriptors.
+
+    :param None
+
+    :return None
+    """
+
+    pass
+
+if __name__ == "__main__":
+    main()
